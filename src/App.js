5a1c90522d4d0dd781b85911c9989308fe342a5d
Paul Okuda <pokuda@palantir.com>
2019-06-19 02:13:26 +0100

    Parallax the client cards

--- src/App.js
+++ src/App.js
@@ -23,8 +23,6 @@ function App() {
              || (rect.x > window.innerWidth || rect.y > window.innerHeight)
     );
   }
-
-  console.log("state", isLandingInView);
   
   return (
     <>
@@ -77,46 +75,58 @@ function App() {
               My name is Steph Jeong, and I’m a digital designer who specializes in product, but dabbles in all things creative. I was born in Seoul, Korea, where Gangnam Style was based in. In my puberty days, my family moved to a small town in Georgia until I went off to Carnegie Mellon University to study communication design. Now, I’m happily living in Brooklyn, NY.
             </div>
             <div className="about-me__text-section">
-              Currently, I’m a senior designer at Ueno, where I get to work with amazing people. Working with other people brings the best out of me, so if you’re a nice person looking for someone to collaborate with, don’t hesitate to [Say Hi]. If you are interested in learning more about my work, I’d be happy to walk you through some of my projects upon request. 
+              Currently, I’m a senior designer at Ueno, where I get to work with amazing people. Working with other people brings the best out of me, so if you’re a nice person looking for someone to collaborate with, don’t hesitate to <span className="orange-text">[Say Hi]</span>. If you are interested in learning more about my work, I’d be happy to walk you through some of my projects upon request. 
             </div>
             <div className="about-me__text-section">
-              I also just like to make new friends, so feel free to follow me on [Twitter] or [Instagram]. Just a warning: if you’re looking for thoughtful design twitter content, you’ve got the wrong address. My Twitter account is a high-quality curation of dumb shit only. Thanks for stopping by!
+              I also just like to make new friends, so feel free to follow me on <span className="orange-text">[Twitter]</span> or <span className="orange-text">[Instagram]</span>. Just a warning: if you’re looking for thoughtful design twitter content, you’ve got the wrong address. My Twitter account is a high-quality curation of dumb shit only. Thanks for stopping by!
             </div>
           </div>
         </Parallax>
       </ParallaxProvider>
-      <div ref={clientLanding} className="client-landing" style={{ position: isLandingInView ? "fixed" : "relative" }}>
-        <div className="client-project-card client-project-card__04">
-          <div className="client-project-image" />
-          <div className="client-project-number">04</div>
-        </div>
-        <div className="client-project-card client-project-card__01">
-          <div className="client-project-image" />
-          <div className="client-project-number">01</div>
-        </div>
-        <div className="client-project-card client-project-card__02">
-          <div className="client-project-image" />
-          <div className="client-project-number">02</div>
-        </div>
-        <div className="client-project-card client-project-card__03">
-          <div className="client-project-image" />
-          <div className="client-project-number">03</div>
-        </div>
-        <div>CLIENTS &#8595;</div>
-        <div>FACEBOOK</div>
-        <div>Samsung</div>
-        <div>Globo</div>
-        <div>Reuters</div>
-        <div>POND5</div>
-        <div>VISA</div>
-        <div>Redbull</div>
-        <div>Sotheby's</div>
-        <div>Dorsia</div>
-        <div>Bluevine</div>
-        <div>Credit Karma</div>
-        <div>Fetch</div>
-        <div>&You</div>
-        <div className="client-list-message-me">[MESSAGE ME]</div>
+      <div style={{ position: "relative" }}>
+        <ParallaxProvider>
+          <div ref={clientLanding} className="client-landing" style={{ position: isLandingInView ? "fixed" : "relative" }}>
+            <div>CLIENTS &#8595;</div>
+            <div>FACEBOOK</div>
+            <div>Samsung</div>
+            <div>Globo</div>
+            <div>Reuters</div>
+            <div>POND5</div>
+            <div>VISA</div>
+            <div>Redbull</div>
+            <div>Sotheby's</div>
+            <div>Dorsia</div>
+            <div>Bluevine</div>
+            <div>Credit Karma</div>
+            <div>Fetch</div>
+            <div>&You</div>
+            <div className="client-list-message-me">[MESSAGE ME]</div>
+          </div>
+          <Parallax y={["550px", "-530px"]} styleOuter={{ width: "500px", height: "715px", position: "absolute", top: "700px", left: "-75px" }} styleInner={{ background: "#dbe2e5" }}>
+            <div className="client-project-card client-project-card__04">
+              <div className="client-project-image" />
+              <div className="client-project-number">04</div>
+            </div>
+          </Parallax>
+          <Parallax y={["1850px", "-1530px"]} styleOuter={{ width: "500px", height: "715px", position: "absolute", top: "30px", left: "110px" }} styleInner={{ background: "#dbe2e5" }}>
+            <div className="client-project-card client-project-card__01">
+              <div className="client-project-image" />
+              <div className="client-project-number">01</div>
+            </div>
+          </Parallax>
+          <Parallax y={["700px", "-1030px"]} styleOuter={{ width: "500px", height: "715px", position: "absolute", top: "1250px", right: "105px" }} styleInner={{ background: "#dbe2e5" }}>
+            <div className="client-project-card client-project-card__03">
+              <div className="client-project-image" />
+              <div className="client-project-number">03</div>
+            </div>
+          </Parallax>
+          <Parallax y={["700px", "-1030px"]} styleOuter={{ width: "500px", height: "715px", position: "absolute", top: "300px", right: "75px" }} styleInner={{ background: "#dbe2e5" }}>
+            <div className="client-project-card client-project-card__02">
+              <div className="client-project-image" />
+              <div className="client-project-number">02</div>
+            </div>
+          </Parallax>
+        </ParallaxProvider>
       </div>
     </>
   );
